b771e5069f3331b8a8209fc44883bcd7040bce71
Matthew Sung <SungMatt@SungMatt.com>
2023-10-05 20:50:51 +0800

    Update storage

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -26,7 +26,7 @@ public class Remark {
      */
     public Remark(String remark) {
         requireNonNull(remark);
-        checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
+        //checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
         value = remark;
     }
 
